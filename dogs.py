038351eaf1a256ca6552f9a1e0900869715a6f80
ANVod <157865931+ANVod@users.noreply.github.com>
2024-09-11 20:51:27 +0300

    add THE DOG  API
    Работа с THE CAT API

---
+++ dogs.py
@@ -0,0 +1,69 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+import requests
+from googletrans import Translator
+from config import TOKEN, THE_DOG_API_KEY
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+translator = Translator()
+
+def get_dog_breeds():
+    url = "https://api.thedogapi.com/v1/breeds"
+    headers = {"x-api-key": THE_DOG_API_KEY}
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+def get_dog_image_by_breed(breed_id):
+    url = f"https://api.thedogapi.com/v1/images/search?breed_ids={breed_id}"
+    headers = {"x-api-key": THE_DOG_API_KEY}
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    return data[0]['url'] if data else None
+
+def get_breed_info(breed_name):
+    breeds = get_dog_breeds()
+    for breed in breeds:
+        if breed['name'].lower() == breed_name.lower():
+            return breed
+    return None
+
+def translate_text(text, dest_language='en'):
+    translated = translator.translate(text, dest=dest_language)
+    return translated.text
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer("Привет! Какая собака тебя интересует? \n"
+                         "Напиши мне название породы собаки,\n"
+                         "и я пришлю тебе её фото и описание.")
+
+@dp.message()
+async def send_dog_info(message: Message):
+    # Переводим введенное сообщение на английский
+    breed_name_en = translate_text(message.text, 'en')
+    breed_info = get_breed_info(breed_name_en)
+
+    if breed_info:
+        dog_image_url = get_dog_image_by_breed(breed_info['id'])
+        if dog_image_url:
+            info = (
+                f"Порода - {breed_info['name']}\n"
+                f"Описание - {breed_info.get('description', 'Описание отсутствует')}\n"
+                f"Продолжительность жизни - {breed_info.get('life_span', 'неизвестно')} лет"
+            )
+            # Переводим описание обратно на русский
+            translated_info = translate_text(info, 'ru')
+            await message.answer_photo(photo=dog_image_url, caption=translated_info)
+        else:
+            await message.answer("Изображение для этой породы не найдено.")
+    else:
+        await message.answer("Порода не найдена. Попробуйте еще раз.")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
